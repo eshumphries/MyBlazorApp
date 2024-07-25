ebce1c29e796c6b0b9c2aa59e4b86bcff511fe80
eshumphries <eshumphries@outlook.com>
2024-07-25 17:52:40 -0400

    Added and applied a razor component

--- obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6445d06c77afcc39fa232a2cd14e25566fd18a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
